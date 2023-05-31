c58f36ccd14acfa1efdf9cb846708c0f41aa6f80
Bernardo Reyes <bbadilla@dev-10.com>
2023-05-31 10:51:26 -0700

    Fix navbar toggling

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -5,7 +5,12 @@ import Logo from "../images/logo.ico";
 export const Navbar = () => {
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-      <NavLink to="/" className="navbar-brand mx-3">
+      <NavLink
+        to="/"
+        className="navbar-brand mx-3"
+        data-toggle="collapse"
+        data-target="#navbarNav"
+      >
         <img src={Logo} alt="MediTrack Logo" />
       </NavLink>
       <button
@@ -22,22 +27,42 @@ export const Navbar = () => {
       <div className="collapse navbar-collapse" id="navbarNav">
         <ul className="navbar-nav mx-2">
           <li className="nav-item active">
-            <NavLink to="/" className="nav-link">
+            <NavLink
+              to="/"
+              className="nav-link"
+              data-toggle="collapse"
+              data-target="#navbarNav"
+            >
               Home
             </NavLink>
           </li>
           <li className="nav-item">
-            <NavLink to="/login" className="nav-link">
+            <NavLink
+              to="/login"
+              className="nav-link"
+              data-toggle="collapse"
+              data-target="#navbarNav"
+            >
               Log In
             </NavLink>
           </li>
           <li className="nav-item">
-            <NavLink to="/signup" className="nav-link">
+            <NavLink
+              to="/signup"
+              className="nav-link"
+              data-toggle="collapse"
+              data-target="#navbarNav"
+            >
               Sign Up
             </NavLink>
           </li>
           <li className="nav-item">
-            <NavLink to="/prescriptions" className="nav-link">
+            <NavLink
+              to="/prescriptions"
+              className="nav-link"
+              data-toggle="collapse"
+              data-target="#navbarNav"
+            >
               Prescriptions
             </NavLink>
           </li>
